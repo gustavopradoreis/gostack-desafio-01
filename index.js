35100baf192379b3ab61c5faa3432b54b69861ce
gustavopradoreis <henrique.gustavoreis@gmail.com>
2019-08-20 14:58:04 -0300

    other routes

--- index.js
+++ index.js
@@ -25,7 +25,16 @@ function projectExists(req, res, next) {
 
 // Routes
 server.post('/projects', (req, res) => {
+    const { id, title } = req.body;
 
+    const project = {
+        id, title,
+        tasks: []
+    }
+
+    projects.push(project);
+        
+    return res.json(projects);
 })
 
 server.get('/projects', (req, res) => {
@@ -33,15 +42,28 @@ server.get('/projects', (req, res) => {
 })
 
 server.put('/projects/:id', projectExists, (req, res) => {
-    return res.json(projects.find(project => project.id === req.params.id));
+    const project = projects.find(project => project.id === req.params.id);
+
+    project.title = req.body.title;
+
+    return res.json(project);
 })
 
 server.delete('/projects/:id', projectExists, (req, res) => {
+    const projectIndex = projects.findIndex(project => project.id === req.params.id);
 
+    projects.splice(projectIndex, 1);
+
+    return res.send();
 })
 
 server.post('/projects/:id/tasks', projectExists, (req, res) => {
+    const { title: task } = req.body;
+    const project = projects.find(project => project.id === req.params.id);
 
+    project.tasks = [...project.tasks, task];
+
+    return res.json(project);
 })
 
 // Server start
